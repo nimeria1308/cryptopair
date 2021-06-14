7024fdfd646a9a0d7f020e53549250ccc9c61123
Simona Dimitrova <nimeria1308@gmail.com>
2021-06-14 22:46:27 +0300

    added get_currencies

--- python/model.py
+++ python/model.py
@@ -1,14 +1,24 @@
 from database import connect_db
 
+LIST_CURRENCIES_SQL = r"SELECT * FROM `currencies`"
+
 LIST_PAIRS_SQL = r"SELECT * FROM `pairs`"
 
 INSERT_BID_SQL = r"INSERT INTO `bids` (`id`, `pair`, `timestamp`, `bid`) VALUES (NULL, '%d', current_timestamp(), '%f')"
 
 
-def model_get_pairs(conn):
+def _model_list(conn, sql):
     with conn.cursor() as cursor:
-        cursor.execute(LIST_PAIRS_SQL)
+        cursor.execute(sql)
         return {c[0]: c[1:] for c in cursor}
+
+
+def model_get_currencies(conn):
+    return _model_list(conn, LIST_CURRENCIES_SQL)
+
+
+def model_get_pairs(conn):
+    return _model_list(conn, LIST_PAIRS_SQL)
 
 
 def model_insert_bid(conn, pair_id, bid):
