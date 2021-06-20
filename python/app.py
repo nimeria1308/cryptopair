636ff0a89d5221b66228ad51e34ef7bff28aa3c3
Simona Dimitrova <nimeria1308@gmail.com>
2021-06-20 22:34:09 +0300

    added currencies and pairs JSON APIs

--- python/app.py
+++ python/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, send_from_directory
+from database import connect_db
+from model import model_get_currencies, model_get_pairs
+
+import json
 import os
 
 app = Flask(__name__, static_folder=os.path.abspath("."))
@@ -14,10 +18,16 @@ def send_resource(path):
     print("static: %s" % path)
     return send_from_directory("res", path)
 
-# API used for requesting data from database
+
+@app.route("/api/currencies")
+def currencies():
+    with connect_db() as conn:
+        currencies = model_get_currencies(conn)
+        return json.dumps(currencies)
 
 
-@app.route("/test")
-def test_r():
-    print("test")
-    return "hello???"
+@app.route("/api/pairs")
+def pairs():
+    with connect_db() as conn:
+        pairs = model_get_pairs(conn)
+        return json.dumps(pairs)
