1ddf0c0c5d27ec6664f0f1137e7a3891e0b6ae86
Simona Dimitrova <nimeria1308@gmail.com>
2021-06-23 20:42:25 +0300

    insert SQL statements directly in functions

--- python/model.py
+++ python/model.py
@@ -1,18 +1,5 @@
 from database import connect_db
 
-LIST_CURRENCIES_SQL = "SELECT * FROM `currencies`"
-
-LIST_PAIRS_SQL = "SELECT * FROM `pairs`"
-
-INSERT_BID_SQL = "INSERT INTO `bids` (`id`, `pair`, `timestamp`, `bid`) VALUES (NULL, '%d', %s, '%f')"
-
-FIND_PAIR_SQL = """
-SELECT `pairs`.`id`, `pairs`.`kraken` FROM `pairs`
-INNER JOIN `currencies` AS c1 ON `pairs`.`base`=`c1`.`id`
-INNER JOIN `currencies` AS c2 ON `pairs`.`quote`=`c2`.`id`
-WHERE `c1`.`name` = '%s' AND `c2`.`name` = '%s'
-"""
-
 
 def _model_list(conn, sql):
     with conn.cursor() as cursor:
@@ -21,21 +8,29 @@ def _model_list(conn, sql):
 
 
 def model_get_currencies(conn):
-    return _model_list(conn, LIST_CURRENCIES_SQL)
+    return _model_list(conn, "SELECT * FROM `currencies`")
 
 
 def model_get_pairs(conn):
-    return _model_list(conn, LIST_PAIRS_SQL)
+    return _model_list(conn, "SELECT * FROM `pairs`")
 
 
 def model_find_pair(conn, base_name, quote_name):
     with conn.cursor() as cursor:
-        sql = FIND_PAIR_SQL % (base_name, quote_name)
+        sql = """
+            SELECT `pairs`.`id`, `pairs`.`kraken` FROM `pairs`
+            INNER JOIN `currencies` AS c1 ON `pairs`.`base`=`c1`.`id`
+            INNER JOIN `currencies` AS c2 ON `pairs`.`quote`=`c2`.`id`
+            WHERE `c1`.`name` = '%s' AND `c2`.`name` = '%s';
+        """ % (base_name, quote_name)
         cursor.execute(sql)
         return [c for c in cursor][0]
 
 
 def model_insert_bid(conn, pair_id, bid, timestamp="current_timestamp()"):
     with conn.cursor() as cursor:
-        insert_sql = INSERT_BID_SQL % (pair_id, timestamp, bid)
-        cursor.execute(insert_sql)
+        sql = """
+            INSERT INTO `bids` (`id`, `pair`, `timestamp`, `bid`)
+            VALUES (NULL, '%d', %s, '%f');
+        """ % (pair_id, timestamp, bid)
+        cursor.execute(sql)
